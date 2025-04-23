7718babe05c02574d08af1d7396f6279e7473954
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-04-23 15:59:28 +0200

    prepare the connection file

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $databaseName = "cafteria_db";
+    
+    $myConnection = new mysqli($serverName, $userName, $password, $databaseName);
+?>
