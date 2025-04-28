416f82be1ef4db93a21b392b116f943cd2433336
Marwa Nasser <marwa.nasser8133@gmail.com>
2025-04-28 19:26:37 +0300

    fix-login-register-forgot-password

--- user/reset_password.php
+++ user/reset_password.php
@@ -1,6 +1,13 @@
 <?php
 include_once('../connect.php');
 
+require_once '../PHPMailer/Exception.php';
+require_once '../PHPMailer/PHPMailer.php';
+require_once '../PHPMailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = mysqli_real_escape_string($myConnection, $_POST['email']);
     $newPassword = $_POST['new_password'];
@@ -34,10 +41,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $updateQuery = "UPDATE users SET password='$hashedPassword' WHERE email='$email'";
 
     if (mysqli_query($myConnection, $updateQuery)) {
-        $subject = "Your password has been reset";
-        $message = "Hello,\n\nYour password has been successfully reset. If you didn't request this change, please contact us immediately.";
-        $headers = "From: marwa.nasser8133@gmail.com";
-        mail($email, $subject, $message, $headers);
+
+        $subject = "Password Reset Confirmation";
+        $message = "
+        <html>
+        <head>
+            <title>Password Reset Successful</title>
+        </head>
+        <body>
+            <h2>Password Reset Successful</h2>
+            <p>Hello,</p>
+            <p>Your password has been successfully reset. If you didn't request this change, please contact our support team immediately.</p>
+            <p>Thank you,<br>Your App Team</p>
+        </body>
+        </html>
+        ";
+
+        $mail = new PHPMailer(true);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'node.app8133@gmail.com';
+            $mail->Password = 'slay zlev kvio bnvi';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+            $mail->setFrom('node.app8133@gmail.com', 'Cafetria');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+
+            $mail->send();
+        } catch (Exception $e) {
+            error_log("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+            header("Location: forgot_password.php?error=mail_failed");
+            exit();
+        }
 
         header("Location: login.php?reset_success=1");
         exit();
